2e85a3d37886b9e948aafc4da1bd13b7c789e947
junjun-creator <kimbj910210@naver.com>
2020-11-28 00:43:31 +0900

    admin userinfo modifying

--- src/com/teum/entity/Member.java
+++ src/com/teum/entity/Member.java
@@ -1,5 +1,113 @@
 package com.teum.entity;
 
+import java.util.Date;
+
 public class Member {
+	private int id;
+	private String name;
+	private String email;
+	private String pwd;
+	private String birthday;
+	private String phone;
+	private int type;
+	private int rank;
+	private Date regdate;
+	
+	public Member() {
+		
+	}
+	
+	public Member(int id, String name, String email, String pwd, String birthday, String phone, int type, int rank,
+			Date regdate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.pwd = pwd;
+		this.birthday = birthday;
+		this.phone = phone;
+		this.type = type;
+		this.rank = rank;
+		this.regdate = regdate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", email=" + email + ", pwd=" + pwd + ", birthday=" + birthday
+				+ ", phone=" + phone + ", type=" + type + ", rank=" + rank + ", regdate=" + regdate + "]";
+	}
+	
 	
 }
--- src/com/teum/service/MemberService.java
+++ src/com/teum/service/MemberService.java
@@ -1,5 +1,68 @@
 package com.teum.service;
 
-public class MemberService {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import com.teum.entity.Member;
+
+public class MemberService {
+	public List<Member> getList(int t) {
+		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
+		String dbid = "TEUM";
+		String dbpwd = "4444";
+		//String sql = "SELECT * FROM MEMBER";
+		
+		List<Member> list = new ArrayList<>();
+		//DriverManager;//Class.forName~
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,dbid,dbpwd);
+			String sql = "SELECT * FROM MEMBER WHERE TYPE = ?";
+			//Statement st = con.createStatement();
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1,t);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String pwd = rs.getString("pwd");
+				String birthday = rs.getString("birthday");
+				String phone = rs.getString("phone");
+				int type = rs.getInt("type");
+				int rank = rs.getInt("rank");
+				Date regdate = rs.getDate("regdate");
+				Member m = new Member();
+				m.setId(id);
+				m.setName(name);
+				m.setEmail(email);
+				m.setPwd(pwd);
+				m.setBirthday(birthday);
+				m.setPhone(phone);
+				m.setType(type);
+				m.setRank(rank);
+				m.setRegdate(regdate);
+				
+				list.add(m);
+			}
+			
+			rs.close();
+			ps.close();
+			con.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 }
