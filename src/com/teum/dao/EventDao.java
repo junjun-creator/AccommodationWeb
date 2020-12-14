9deb4bbf4f0c3c39730c64130cb7605be8d535d6
JaeHee Cho <40957996+cbw1030@users.noreply.github.com>
2020-12-14 16:38:50 +0900

    controller 에러 수정

--- src/com/teum/dao/EventDao.java
+++ src/com/teum/dao/EventDao.java
@@ -2,8 +2,11 @@ package com.teum.dao;
 
 import java.util.List;
 
+import com.teum.dao.entity.EventView;
 import com.teum.entity.Event;
 
+// SQL의 자바 함수화(정렬, 그룹핑, 필터링)
+// 업무는 모른다.
 public interface EventDao {
 
 	int insert(Event event);
@@ -11,5 +14,11 @@ public interface EventDao {
 	int delete(int id);
 	
 	Event get(int id);
+	List<Event> getList(int startIndex, int endIndex, String query);
+	List<Event> getList(int startIndex, int endIndex);
 	List<Event> getList();
+	
+	List<EventView> getViewList(int startIndex, int endIndex, String query);
+	List<EventView> getViewList(int startIndex, int endIndex);
+	List<EventView> getViewList();
 }
