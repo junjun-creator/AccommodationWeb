2d76be8b996753cd8347cfb3823a2bf2f879d747
unknown <flight.logan@gmail.com>
2020-12-14 16:55:13 +0900

    12.14

--- src/com/teum/controller/admin/acc/ListController.java
+++ src/com/teum/controller/admin/acc/ListController.java
@@ -1,6 +1,7 @@
 package com.teum.controller.admin.acc;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import com.teum.entity.AccommodationList;
+import com.teum.entity.Acc;
 import com.teum.service.AccService;
 
 @WebServlet("/admin/accommodations/list")
@@ -18,9 +19,10 @@ public class ListController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		AccService service = new AccService();
-//		List<AccommodationListServiceI> list = service.getList();
+		List<Acc> list = new ArrayList<>();
 		
-//		request.setAttribute("list", list);
+		
+		request.setAttribute("list", list);
 		request.getRequestDispatcher("list.jsp").forward(request, response);
 	}
 }
--- src/com/teum/service/AccService.java
+++ src/com/teum/service/AccService.java
@@ -1,239 +1,80 @@
-//package com.teum.service;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//
-//import com.teum.entity.AccommodationList;
-//
-//public class AccService {
-//
-//	   public List<AccommodationListServiceI> getList() {
-//		      String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-//		      String sql = "SELECT * FROM ACCOMMODATION_LIST WHERE REGISTRATION ='1'";
-//		      
-//		      List<AccommodationListServiceI> list = new ArrayList<>();
-//		      try {
-//		         Class.forName("oracle.jdbc.driver.OracleDriver");
-//		         Connection con = DriverManager.getConnection(url,"TEUM","4444");
-//		         Statement st = con.createStatement();
-//		         ResultSet rs = st.executeQuery(sql);
-//		         
-//		      
-//		         
-//		         
-//		         while(rs.next()) {
-//		         
-//		            int id = rs.getInt("id"); 
-//		            String name = rs.getString("name");
-//		            String accName = rs. getString("acc_name");
-//		            String location = rs.getString("location");
-//		            String regdate = rs.getString("regdate");
-//		            String phone = rs.getString("phone");
-//		            String email = rs. getString("email");
-//		            String accType = rs.getString("acc_type");
-//		            int registration = rs.getInt("registration");
-//		            
-//		            AccommodationListServiceI al = new AccommodationListServiceI(
-//		                  id,
-//		                  name,
-//		                  accName,
-//		                  location,
-//		                  regdate,
-//		                  phone,
-//		                  email,
-//		                  accType,
-//		                  registration
-//		            );
-//		            
-//		            
-//		            list.add(al);
-//		         };
-//		         
-//		                  
-//		         rs.close();
-//		         st.close();
-//		         con.close();
-//		         
-//		      } catch (SQLException e) {
-//		         // TODO Auto-generated catch block
-//		         e.printStackTrace();
-//		      } catch (ClassNotFoundException e) {
-//		         // TODO Auto-generated catch block
-//		         e.printStackTrace();
-//		      }
-//		      return list;
-//		   }
-//
-//	public AccommodationListServiceI get(String accName) {
-//		AccommodationListServiceI al = null;
-//		
-//		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-//	    String sql = "SELECT * FROM ACCOMMODATION_LIST WHERE REGISTRATION ='1'";//조건 추가(accName)
-//	    
-//	      try {
-//	         Class.forName("oracle.jdbc.driver.OracleDriver");
-//	         Connection con = DriverManager.getConnection(url,"TEUM","4444");
-//	         Statement st = con.createStatement();
-//	         ResultSet rs = st.executeQuery(sql);
-//	         
-//	      
-//	         
-//	         
-//	         while(rs.next()) {
-//	         
-//	            int id = rs.getInt("id"); 
-//	            String name = rs.getString("name");
-//	            String location = rs.getString("location");
-//	            String regdate = rs.getString("regdate");
-//	            String phone = rs.getString("phone");
-//	            String email = rs. getString("email");
-//	            String accType = rs.getString("acc_type");
-//	            int registration = rs.getInt("registration");
-//	            
-//	             al = new AccommodationListServiceI(
-//	                  id,
-//	                  name,
-//	                  accName,
-//	                  location,
-//	                  regdate,
-//	                  phone,
-//	                  email,
-//	                  accType,
-//	                  registration
-//	            );
-//
-//	         };
-//	         
-//	                  
-//	         rs.close();
-//	         st.close();
-//	         con.close();
-//	         
-//	      } catch (SQLException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      } catch (ClassNotFoundException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      }
-//		return al;
-//	}
-//	
-//	public List<AccommodationListServiceI> applyGetList() {
-//	      String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-//	      String sql = "SELECT * FROM ACCOMMODATION_LIST WHERE REGISTRATION ='0'";
-//	      Connection con;
-//	      List<AccommodationListServiceI> list = new ArrayList<>();
-//	      try {
-//	         Class.forName("oracle.jdbc.driver.OracleDriver");
-//	         con = DriverManager.getConnection(url,"TEUM","4444");
-//	         Statement st = con.createStatement();
-//	         ResultSet rs = st.executeQuery(sql);
-//	         
-//	      
-//	         
-//	         
-//	         while(rs.next()) {
-//	         
-//	            int id = rs.getInt("id"); 
-//	            String name = rs.getString("name");
-//	            String accName = rs. getString("acc_name");
-//	            String location = rs.getString("location");
-//	            String regdate = rs.getString("regdate");
-//	            String phone = rs.getString("phone");
-//	            String email = rs. getString("email");
-//	            String accType = rs.getString("acc_type");
-//	            int registration = rs.getInt("registration");
-//	            
-//	            AccommodationListServiceI al = new AccommodationListServiceI(
-//	                  id,
-//	                  name,
-//	                  accName,
-//	                  location,
-//	                  regdate,
-//	                  phone,
-//	                  email,
-//	                  accType,
-//	                  registration
-//	            );
-//	            
-//	            
-//	            list.add(al);
-//	         };
-//	         
-//	                  
-//	         rs.close();
-//	         st.close();
-//	         con.close();
-//	         
-//	      } catch (SQLException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      } catch (ClassNotFoundException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      }
-//	      return list;
-//	   }
-//	
-//	public AccommodationListServiceI applyGet(String accName) {
-//		AccommodationListServiceI al = null;
-//		
-//		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-//	    String sql = "SELECT * FROM ACCOMMODATION_LIST WHERE REGISTRATION ='0'";//조건 추가(accName)
-//	    
-//	      try {
-//	         Class.forName("oracle.jdbc.driver.OracleDriver");
-//	         Connection con = DriverManager.getConnection(url,"TEUM","4444");
-//	         Statement st = con.createStatement();
-//	         ResultSet rs = st.executeQuery(sql);
-//	         
-//	      
-//	         
-//	         
-//	         while(rs.next()) {
-//	         
-//	            int id = rs.getInt("id"); 
-//	            String name = rs.getString("name");
-//	            String location = rs.getString("location");
-//	            String regdate = rs.getString("regdate");
-//	            String phone = rs.getString("phone");
-//	            String email = rs. getString("email");
-//	            String accType = rs.getString("acc_type");
-//	            int registration = rs.getInt("registration");
-//	            
-//	             al = new AccommodationListServiceI(
-//	                  id,
-//	                  name,
-//	                  accName,
-//	                  location,
-//	                  regdate,
-//	                  phone,
-//	                  email,
-//	                  accType,
-//	                  registration
-//	            );
-//
-//	         };
-//	         
-//	                  
-//	         rs.close();
-//	         st.close();
-//	         con.close();
-//	         
-//	      } catch (SQLException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      } catch (ClassNotFoundException e) {
-//	         // TODO Auto-generated catch block
-//	         e.printStackTrace();
-//	      }
-//		return al;
-//	}
-//}
+package com.teum.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.teum.dao.AccDao;
+import com.teum.dao.jdbc.DBContext;
+//import com.teum.dao.jdbc.JdbcAccDao;
+import com.teum.entity.Acc;
+import com.teum.entity.AccListForAdminView;
+
+
+public class AccService {
+	
+	private AccDao accDao;
+	public AccService() {
+//		accDao = new JdbcAccDao();
+	}
+	
+	public List<Acc> getList(int page, int size, String accType, String qeury) {
+		
+		int startIndex =1+(page-1)*size;//1, 11, 21, 31, ...
+		int endIndex = page*10;//10,20,30,40,50,60... 
+		
+		return accDao.getList(startIndex, endIndex, accType, qeury);
+	}
+	public List<AccListForAdminView> getViewList(int page, int size, String accType, String qeury) {
+		
+		int startIndex =1+(page-1)*size;//1, 11, 21, 31, ...
+		int endIndex = page*10;//10,20,30,40,50,60... 
+		
+		return accDao.getViewList(startIndex, endIndex, accType, qeury);
+	}
+
+	public Acc get(int id) {
+		return accDao.get(id);
+	}
+
+	public List<Acc> applyGetList(int page, int size, String accType, String qeury) {
+		
+		int startIndex =1+(page-1)*size;//1, 11, 21, 31, ...
+		int endIndex = page*10;//10,20,30,40,50,60... 
+		
+		return accDao.applyGetList(startIndex, endIndex, accType, qeury);
+	}
+
+	public Acc applyGet(int id) {
+		return accDao.applyGet(id);
+	}
+	
+	public int delete(int id) {
+		int result=0;
+		result = accDao.delete(id);
+		
+		return result;
+	}
+	//수정필요
+	public int[] deleteAll(int[] ids) {
+		
+		int result =0;
+		for(int i=0; i<ids.length; i++) {
+			int id = ids[i];
+			result+= accDao.delete(id);
+		}
+		return null;
+	}
+
+	public int update(Acc acc) {
+		int result = 0;
+		
+		result = accDao.update(acc);
+		return result;
+	}
+}
