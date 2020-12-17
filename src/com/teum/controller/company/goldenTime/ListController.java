b86abf18ce714cc0d350a74c0aebaf45176ef161
unknown <suhyeon7846@gmail.com>
2020-12-18 02:16:56 +0900

    acc goldentimeservice 생성

--- src/com/teum/controller/company/goldenTime/ListController.java
+++ src/com/teum/controller/company/goldenTime/ListController.java
@@ -1,12 +1,17 @@
 package com.teum.controller.company.goldenTime;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.teum.dao.entity.GoldenTimeView;
+import com.teum.service.AccService;
 
 @WebServlet("/company/goldenTime/list")
 public class ListController extends HttpServlet {
@@ -14,6 +19,13 @@ public class ListController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
+	HttpSession session = request.getSession();	
 		
+	
+	AccService service = new AccService();
+	List<GoldenTimeView> list = service.getGoldenList(); 
+	
+	request.setAttribute("list", list);
+	request.getRequestDispatcher("list.jsp").forward(request, response);
 	}
 }
--- src/com/teum/dao/entity/GoldenTimeView.java
+++ src/com/teum/dao/entity/GoldenTimeView.java
@@ -1,10 +1,38 @@
 package com.teum.dao.entity;
 
-public class GoldenTimeView {
-	private String accName;
-	private String price;
-	private String salePrice;
-	private String location;
-	//보류
+import java.util.Date;
+
+import com.teum.entity.Acc;
+
+public class GoldenTimeView extends Acc {
+	private String fileroute;
+	
+	public GoldenTimeView() {
+		
+	}
+
+	public GoldenTimeView(int id, String name, String phone, String location, int regStatus, Date approvalDate,
+			int adminId, int companyId, Date regdate, int accTypeId, Date gtStartDate, Date gtEndDate, int saleprice,
+			int goldentimeStatus, String fileroute) {
+		super(id, name, phone, location, regStatus, approvalDate, adminId, companyId, regdate, accTypeId, gtStartDate,
+				gtEndDate, saleprice, goldentimeStatus);
+		this.fileroute=fileroute;
+	}
+
+	public String getFileroute() {
+		return fileroute;
+	}
+
+	public void setFileroute(String fileroute) {
+		this.fileroute = fileroute;
+	}
+
+	@Override
+	public String toString() {
+		return "GoldenTimeView [fileroute=" + fileroute + "]";
+	}
+	
+	
+	
 	
 }
